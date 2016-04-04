d4e5a18c8d2060eaeb1db3533e0c8c1f8cc27fec
TeunVL <teun.vanlingen@hotmail.com>
2016-04-04 11:40:18 +0200

    -removed all code that was used for the four led states

--- proto_ledsjump/proto_ledsjump.ino
+++ proto_ledsjump/proto_ledsjump.ino
@@ -1,12 +1,12 @@
 #define UP true
 #define DOWN false
 
-int ledsPin[] = {2,3,4,5};
-
-long ledsOneScore = 0;
-long ledsTwoScore = 0;
-long ledsThreeScore = 0;
-long ledsFourScore = 0;
+//int ledsPin[] = {2,3,4,5};
+//
+//long ledsOneScore = 0;
+//long ledsTwoScore = 0;
+//long ledsThreeScore = 0;
+//long ledsFourScore = 0;
 
 //pressure sensor
 int analogButton = A0;
@@ -25,36 +25,36 @@ long totalScoreRecal = 0;      //totalScore recalculated on a scale from 0 to 10
 long jumpScoreRecal = 0;
 float maxScore = 0;
 
-int ledWaveSpeed = 50;                  //speed of shockwave leds, high is slow, low is fast
+//int ledWaveSpeed = 50;                  //speed of shockwave leds, high is slow, low is fast
 bool session = false;                   //is true when user has start jumping, is false when sessionTime is reached and last jump is made
 unsigned long previousSessionCounter = 0;
 const long sessionTime = 30000;         //sets the session time in mili-secs
 int waitTime = 500;                     //sets the time after session to wait till next session can begin
 
-int ledIdleSpeedSet = 1;
-int ledIdleSpeedPrimary = 1000;                 //first speed of leds switching when session is false
-int ledIdleSpeedSecondary = 100;                  //secondary speed of leds switching when session is false 
-bool ledDirection = UP;                  //defines if leds or moving up or down when idle
-int ledState = 0x01;                     //specifies which led turns on and of
-unsigned long previousIdleCounter = 0;
+//int ledIdleSpeedSet = 1;
+//int ledIdleSpeedPrimary = 1000;                 //first speed of leds switching when session is false
+//int ledIdleSpeedSecondary = 100;                  //secondary speed of leds switching when session is false 
+//bool ledDirection = UP;                  //defines if leds or moving up or down when idle
+//int ledState = 0x01;                     //specifies which led turns on and of
+//unsigned long previousIdleCounter = 0;
 
 void setup() {
   Serial.begin(9600);
-  for (int i=0; i<4; i++){
-    pinMode(ledsPin[i],OUTPUT);
-  }
+//  for (int i=0; i<4; i++){
+//    pinMode(ledsPin[i],OUTPUT);
+//  }
 
-  ledsOff();
+//  ledsOff();
 
   //specifies when the leds react at the score of the player
   jumpMaxScore = pow(jumpMaxTime,2);
   maxJumpTimes = sessionTime/jumpMaxTime;
   maxScore = pow(jumpMaxTime,2) * maxJumpTimes;
-  long eightMaxScore = maxScore/8;
-  ledsOneScore = 1 * eightMaxScore;
-  ledsTwoScore = 3 * eightMaxScore;
-  ledsThreeScore = 5 * eightMaxScore;
-  ledsFourScore = 7 * eightMaxScore;
+//  long eightMaxScore = maxScore/8;
+//  ledsOneScore = 1 * eightMaxScore;
+//  ledsTwoScore = 3 * eightMaxScore;
+//  ledsThreeScore = 5 * eightMaxScore;
+//  ledsFourScore = 7 * eightMaxScore;
 }
 
 void loop() {
@@ -68,7 +68,7 @@ void loop() {
     else {presureState = HIGH;}
   
     if (presureState == HIGH && previousPresureState == LOW){
-      jumpHitLeds();
+//      jumpHitLeds();
       previousSessionCounter = currentCounter;
       session = true;
       previousjumpTime = currentCounter;
@@ -77,25 +77,11 @@ void loop() {
       session = false;
     }
 
-    
-//    if (ledIdleSpeedSet == 1){
-//      for (int i=0; i <3; i++){
-        if (currentCounter - previousIdleCounter >= ledIdleSpeedPrimary) {
-          previousIdleCounter = currentCounter;
-          idle();
-        }
-//      }
-//      ledIdleSpeedSet = 2;
-//    }
-//    if (ledIdleSpeedSet == 2){
-//      for (int i=0; i <1; i++){
-//        if (currentCounter - previousIdleCounter >= ledIdleSpeedSecondary) {
-//          previousIdleCounter = currentCounter;
-//          idle();
-//        }
-//      }
-//      ledIdleSpeedSet = 1;
-//    }
+    if (currentCounter - previousIdleCounter >= ledIdleSpeedPrimary) {
+      previousIdleCounter = currentCounter;
+      idle();
+    }
+
   }
 
   //start of the session
@@ -122,8 +108,8 @@ void loop() {
       Serial.print(" total score: ");
       Serial.println(totalScoreRecal);
 
-      jumpHitLeds();
-      scoreLeds();
+//      jumpHitLeds();
+//      scoreLeds();
       
       if(currentCounter - previousSessionCounter >= sessionTime){
         restartGame();
@@ -137,60 +123,60 @@ void loop() {
   previousPresureState = presureState;
 }
 
-void jumpHitLeds(){
-  ledsOff();
-  for (int i=0; i<5; i++){
-    digitalWrite(ledsPin[i],HIGH);
-    delay(ledWaveSpeed);
-    digitalWrite(ledsPin[i],LOW);
-  }
-}
+//void jumpHitLeds(){
+//  ledsOff();
+//  for (int i=0; i<5; i++){
+//    digitalWrite(ledsPin[i],HIGH);
+//    delay(ledWaveSpeed);
+//    digitalWrite(ledsPin[i],LOW);
+//  }
+//}
 
-void scoreLeds(){
-  if (totalScore > ledsFourScore){
-    digitalWrite(ledsPin[0], HIGH);
-    digitalWrite(ledsPin[1], HIGH);
-    digitalWrite(ledsPin[2], HIGH);
-    digitalWrite(ledsPin[3], HIGH);
-  }
-  else if (totalScore > ledsThreeScore){
-    digitalWrite(ledsPin[0], HIGH);
-    digitalWrite(ledsPin[1], HIGH);
-    digitalWrite(ledsPin[2], HIGH);
-    digitalWrite(ledsPin[3], LOW);
-  }
-  else if (totalScore > ledsTwoScore){
-    digitalWrite(ledsPin[0], HIGH);
-    digitalWrite(ledsPin[1], HIGH);
-    digitalWrite(ledsPin[2], LOW);
-    digitalWrite(ledsPin[3], LOW);
-  }
-  else if (totalScore > ledsOneScore){
-    digitalWrite(ledsPin[0], HIGH);
-    digitalWrite(ledsPin[1], LOW);
-    digitalWrite(ledsPin[2], LOW);
-    digitalWrite(ledsPin[3], LOW);
-  }
-  else {
-    ledsOff();
-  }
-}
+//void scoreLeds(){
+//  if (totalScore > ledsFourScore){
+//    digitalWrite(ledsPin[0], HIGH);
+//    digitalWrite(ledsPin[1], HIGH);
+//    digitalWrite(ledsPin[2], HIGH);
+//    digitalWrite(ledsPin[3], HIGH);
+//  }
+//  else if (totalScore > ledsThreeScore){
+//    digitalWrite(ledsPin[0], HIGH);
+//    digitalWrite(ledsPin[1], HIGH);
+//    digitalWrite(ledsPin[2], HIGH);
+//    digitalWrite(ledsPin[3], LOW);
+//  }
+//  else if (totalScore > ledsTwoScore){
+//    digitalWrite(ledsPin[0], HIGH);
+//    digitalWrite(ledsPin[1], HIGH);
+//    digitalWrite(ledsPin[2], LOW);
+//    digitalWrite(ledsPin[3], LOW);
+//  }
+//  else if (totalScore > ledsOneScore){
+//    digitalWrite(ledsPin[0], HIGH);
+//    digitalWrite(ledsPin[1], LOW);
+//    digitalWrite(ledsPin[2], LOW);
+//    digitalWrite(ledsPin[3], LOW);
+//  }
+//  else {
+//    ledsOff();
+//  }
+//}
 
 void ledsOff(){
-  for (int i=0; i<5; i++){
-    digitalWrite(ledsPin[i], LOW);
-  }
+//  for (int i=0; i<5; i++){
+//    digitalWrite(ledsPin[i], LOW);
+//  }
 }
 
 void restartGame(){
   Serial.print("Je eind score = ");
   Serial.println(totalScoreRecal);
-  for (int i=0; i<7; i++){
-    scoreLeds();
-    delay(waitTime);
-    ledsOff();
-    delay(waitTime);
-  }
+//  for (int i=0; i<7; i++){
+//    scoreLeds();
+//    delay(waitTime);
+//    ledsOff();
+//    delay(waitTime);
+//  }
   jumpScore = 0;
   totalScore = 0;
   session = false;
@@ -200,28 +186,28 @@ void restartGame(){
 void idle(){
   // Set the pins of each LED during each iteration
   // You'll only see something change when "LEDpins" gets updated
-  for (int x=0; x < 4; x++)
-    digitalWrite(ledsPin[x], bitRead(ledState,x));
-    
-    if (ledDirection==UP) {
-      // Use "<<" to "bit-shift" everything to the left once
-      ledState = ledState << 1;
-      // 0x20 is the "last" LED, another shift makes the value 0x40
-      if (ledState == 0x08) {
-        // turn on the one before "0x20" and reverse direction
-        ledState = 0x08;
-        ledDirection = DOWN;
-      }
-    }
-    else {
-      // use ">>" to "bit-shift" all bits in LEDstate once to the right
-      ledState = ledState >> 1;
-      // This means we ran out of bits!
-      if (ledState == 0x00) {
-        // set one ahead so no extra delay
-        ledState = 0x02;
-        ledDirection = UP;
-      }
-    }
+//  for (int x=0; x < 4; x++)
+//    digitalWrite(ledsPin[x], bitRead(ledState,x));
+//    
+//    if (ledDirection==UP) {
+//      // Use "<<" to "bit-shift" everything to the left once
+//      ledState = ledState << 1;
+//      // 0x20 is the "last" LED, another shift makes the value 0x40
+//      if (ledState == 0x08) {
+//        // turn on the one before "0x20" and reverse direction
+//        ledState = 0x08;
+//        ledDirection = DOWN;
+//      }
+//    }
+//    else {
+//      // use ">>" to "bit-shift" all bits in LEDstate once to the right
+//      ledState = ledState >> 1;
+//      // This means we ran out of bits!
+//      if (ledState == 0x00) {
+//        // set one ahead so no extra delay
+//        ledState = 0x02;
+//        ledDirection = UP;
+//      }
+//    }
 }
 
